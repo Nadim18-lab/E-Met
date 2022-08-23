1342dc730f77b609906364f0754567659ff2039e
Nadim18-lab <nadincho18@gmail.com>
2022-08-23 12:38:41 -0300

    1st commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import NavBar from './components/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <div className="App">
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
