55642084114f9b7f8c1977c74f5d49062158d0e3
Nadim18-lab <nadincho18@gmail.com>
2022-08-25 12:25:59 -0300

    react icon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,13 @@
 import NavBar from './components/NavBar';
 import './App.css';
+import ItemListContainer from './containers/ItemListContainer';
 
 function App() {
   return (
     <>
     <NavBar/>
+    <ItemListContainer greeting={"ALOH"}/>
     <div className="App">
-      
     </div>
     </>
   );
--- my-app/src/components/NavBar/index.jsx
+++ my-app/src/components/NavBar/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Cart from '../CartWidget/Cart'
+import './styles.css'
+
+
+
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    
+         <nav className="navbar navbar-expand-lg navbar-light bg-light menu">
+            <a className="navbar-brand title" href="/#">E-Met</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active"href="/#">Sobre Nosotros</a>
+            <a className="nav-item nav-link active"href="/#">Productos</a>
+            <a className="nav-item nav-link active"href="/#">Contacto</a>
+            <a className="nav-item nav-link active"href="/#"><Cart/></a>
+            <a className="nav-item nav-link active"href="/#">*cambio de color*</a>
+          </div>
+        </div>
+        </nav> 
+
   )
 }
 
