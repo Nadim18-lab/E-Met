d8d220e21149223a93eb6f2ddb854402679371e9
Nadim18-lab <nadincho18@gmail.com>
2022-09-08 10:14:50 -0300

    update cart

--- my-app/src/components/Item/item.jsx
+++ my-app/src/components/Item/item.jsx
@@ -3,13 +3,17 @@ import ItemCount from '../ItemCounter/ItemC'
 import './item.css'
 
 const item = ({vinyl}) => {
+  const onAdd = (count)=>{
+    console.log(`se a agregado ${count} al carrito` );
+  } 
+
   return (
     <div className="card">
         <img className="card-img-top" src={vinyl.img} alt="Card image cap"/>
         <div className="card-body">
             <p className="card-text">{vinyl.genre}</p>
             <p className="card-text">{vinyl.price}</p>
-            <ItemCount stock={vinyl.stock}/>
+            <ItemCount onAdd={onAdd} stock={vinyl.stock}/>
         </div>
 </div>
   )
